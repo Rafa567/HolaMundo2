cceaadd8281d76e73a8dcf3ad5dcf9a697ea1f77
Pontifice Shullyvan <rframosduran7@gmail.com>
2019-04-01 12:13:26 +0200

    Modificada la clase principal

---
+++ src/holamundo2/HolaMundo2.java
@@ -0,0 +1,11 @@
+package holamundo2;
+
+public class HolaMundo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde el programa dos");
+        System.out.println("Abril 2019");
+
+    }
+    
+}
